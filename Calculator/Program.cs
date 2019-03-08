a079a88b59d9f16707b576867a124da0c9e18d7c
Egor <egor.record@ya.ru>
2019-03-08 18:24:56 +0300

    Add some logic

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -22,15 +22,19 @@ namespace Calculator
 
             string history = string.Empty;
 
+            PrintMenu();
+
 
             while (true)
             {
-                PrintMenu();
+               
 
                 string usersInput = Console.ReadLine();
 
                 if (string.Compare( usersInput, "Q", true) == 0)
                 {
+                    Console.WriteLine("Goodbye!");
+
                     break;
                 }
 
@@ -42,6 +46,8 @@ namespace Calculator
 
                         Console.WriteLine("Entered number is not valid");
 
+                        Console.WriteLine("Press Q to exit");
+
                     }
                     else
                     {
@@ -51,19 +57,78 @@ namespace Calculator
 
                         Console.WriteLine("Enter operation type ");
                     }
+
                 } else if (expectedInput == ExpectedInput.Operation)
                 {
+                  
+
+                    if (usersInput.Trim() == "+")
+                    {
+                        history += usersInput.ToString() + " ";
+
+                        expectedInput = ExpectedInput.Operant2;
+
+                        operationType = OperationType.Sum;
+
+                        Console.WriteLine("Enter second operant ");
+
+                    } else if (usersInput.Trim() == "-")
+                    {
+
+                        history += usersInput.ToString() + " ";
+
+                        expectedInput = ExpectedInput.Operant2;
+
+                        operationType = OperationType.Diff;
+
+                        Console.WriteLine("Enter second operant ");
+
+
+                    } else if (usersInput.Trim() == "*")
+                    {
+                        history += usersInput.ToString() + " ";
+
+                        expectedInput = ExpectedInput.Operant2;
+
+                        operationType = OperationType.Prod;
+
+                        Console.WriteLine("Enter second operant ");
+
+                    } else if (usersInput.Trim() == "/")
+                    {
+                        history += usersInput.ToString() + " ";
+
+                        expectedInput = ExpectedInput.Operant2;
+
+                        operationType = OperationType.Div;
+
+                        Console.WriteLine("Enter second operant ");
+
+                    } else
+                    {
+                        Console.WriteLine("Entered operation type is not valid");
+                        Console.WriteLine("Press Q to exit");
+                    }
 
                     // TODO: По аналогии распарсить что введт поль, если плюс, то продолжить, если не плюс то вывести сообщение об ошибки и добиться чтобы в вел нормальный знак
                     // TODO: Второе число после того как польщователь ввел плюс или минус 
                     // TODO: Чтобы ввыводился результат и хистори, где все это сохраняется
 
+                } else if (expectedInput == ExpectedInput.Operant2)
+                {
+
+                    if (!float.TryParse(usersInput, out operand2))
+                    {
+
+                    }
+
+
                 }
 
-                
 
 
-                Console.WriteLine("This is history: " + history);
+
+                 //   Console.WriteLine("This is history: " + history);
             }
 
 
@@ -73,7 +138,7 @@ namespace Calculator
         {
             Console.WriteLine("Please, enter the first number");
             Console.WriteLine("Press Q to exit");
-            Console.WriteLine("Press + to perfome sum");
+         
 
 
         }
